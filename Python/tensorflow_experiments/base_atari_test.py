5c1eec03f928ae8b71fe78de48a4518ce63ea3d1
moraguma <g170603@dac.unicamp.br>
2024-02-06 22:24:10 -0300

    kinda working

--- Python/tensorflow_experiments/base_atari_test.py
+++ Python/tensorflow_experiments/base_atari_test.py
@@ -30,30 +30,46 @@ from tf_agents.trajectories import trajectory
 from tf_agents.specs import tensor_spec
 from tf_agents.utils import common
 
+"""
+Computes the average return of a policy given an environment and number of episodes
+"""
+def compute_avg_return(environment, policy, num_episodes=10):
+
+  total_return = 0.0
+  for _ in range(num_episodes):
+
+    time_step = environment.reset()
+    episode_return = 0.0
+
+    while not time_step.is_last():
+      action_step = policy.action(time_step)
+      time_step = environment.step(action_step.action)
+      episode_return += time_step.reward
+    total_return += episode_return
+
+  avg_return = total_return / num_episodes
+  return avg_return.numpy()[0]
+
+
+# See also the metrics module for standard implementations of different metrics.
+# https://github.com/tensorflow/agents/tree/master/tf_agents/metrics
+
 # Set up a virtual display for rendering OpenAI gym environments.
-display = pyvirtualdisplay.Display(visible=0, size=(1400, 900)).start()
+#display = pyvirtualdisplay.Display(visible=0, size=(1400, 900)).start()
 
 ###################
 # Hyperparameters #
 ###################
-env_name = "CartPole-v1"
-num_iterations = 15000
+print("Hi")
+num_iterations = 20000 
 
-initial_collect_steps = 1000  
-collect_steps_per_iteration = 1 
-replay_buffer_capacity = 100000 
-
-fc_layer_params = (100,)
+initial_collect_steps = 100  
+collect_steps_per_iteration =   1
+replay_buffer_max_length = 100000  
 
 batch_size = 64  
 learning_rate = 1e-3 
-gamma = 0.99
 log_interval = 200  
-
-num_atoms = 51  
-min_q_value = -20  
-max_q_value = 20  
-n_step_update = 2  
 
 num_eval_episodes = 10  
 eval_interval = 1000  
@@ -61,8 +77,145 @@ eval_interval = 1000
 #####################
 # Environment setup #
 #####################
-train_py_env = suite_gym.load(env_name)
-eval_py_env = suite_gym.load(env_name)
+env_name = 'CartPole-v0'
+env = suite_gym.load(env_name)
+
+train_py_env = suite_gym.load(env_name) # For training the model
+eval_py_env = suite_gym.load(env_name)  # For evaluating the model
 
 train_env = tf_py_environment.TFPyEnvironment(train_py_env)
 eval_env = tf_py_environment.TFPyEnvironment(eval_py_env)
+
+###############
+# Agent setup #
+###############
+fc_layer_params = (100, 50)
+action_tensor_spec = tensor_spec.from_spec(env.action_spec())
+num_actions = action_tensor_spec.maximum - action_tensor_spec.minimum + 1
+
+# Define a helper function to create Dense layers configured with the right
+# activation and kernel initializer.
+def dense_layer(num_units):
+  return tf.keras.layers.Dense(
+      num_units,
+      activation=tf.keras.activations.relu,
+      kernel_initializer=tf.keras.initializers.VarianceScaling(
+          scale=2.0, mode='fan_in', distribution='truncated_normal'))
+
+# QNetwork consists of a sequence of Dense layers followed by a dense layer
+# with `num_actions` units to generate one q_value per available action as
+# its output.
+dense_layers = [dense_layer(num_units) for num_units in fc_layer_params]
+q_values_layer = tf.keras.layers.Dense(
+    num_actions,
+    activation=None,
+    kernel_initializer=tf.keras.initializers.RandomUniform(
+        minval=-0.03, maxval=0.03),
+    bias_initializer=tf.keras.initializers.Constant(-0.2))
+q_net = sequential.Sequential(dense_layers + [q_values_layer])
+
+optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+
+train_step_counter = tf.Variable(0)
+
+agent = dqn_agent.DqnAgent(
+    train_env.time_step_spec(),
+    train_env.action_spec(),
+    q_network=q_net,
+    optimizer=optimizer,
+    td_errors_loss_fn=common.element_wise_squared_loss,
+    train_step_counter=train_step_counter)
+
+agent.initialize()
+
+##########
+# Policy #
+##########
+random_policy = random_tf_policy.RandomTFPolicy(train_env.time_step_spec(),
+                                                train_env.action_spec())
+
+#################
+# Replay buffer #
+#################
+table_name = 'uniform_table'
+replay_buffer_signature = tensor_spec.from_spec(
+      agent.collect_data_spec)
+replay_buffer_signature = tensor_spec.add_outer_dim(
+    replay_buffer_signature)
+
+table = reverb.Table(
+    table_name,
+    max_size=replay_buffer_max_length,
+    sampler=reverb.selectors.Uniform(),
+    remover=reverb.selectors.Fifo(),
+    rate_limiter=reverb.rate_limiters.MinSize(1),
+    signature=replay_buffer_signature)
+
+reverb_server = reverb.Server([table])
+
+replay_buffer = reverb_replay_buffer.ReverbReplayBuffer(
+    agent.collect_data_spec,
+    table_name=table_name,
+    sequence_length=2,
+    local_server=reverb_server)
+
+rb_observer = reverb_utils.ReverbAddTrajectoryObserver(
+  replay_buffer.py_client,
+  table_name,
+  sequence_length=2)
+
+# Fill replay buffer with random policy experience and create sampler for agent
+py_driver.PyDriver(
+    env,
+    py_tf_eager_policy.PyTFEagerPolicy(
+      random_policy, use_tf_function=True),
+    [rb_observer],
+    max_steps=initial_collect_steps).run(train_py_env.reset())
+dataset = replay_buffer.as_dataset(
+    num_parallel_calls=3,
+    sample_batch_size=batch_size,
+    num_steps=2).prefetch(3)
+iterator = iter(dataset)
+
+############
+# Training #
+############
+# (Optional) Optimize by wrapping some of the code in a graph using TF function.
+agent.train = common.function(agent.train)
+
+# Reset the train step.
+agent.train_step_counter.assign(0)
+
+# Evaluate the agent's policy once before training.
+avg_return = compute_avg_return(eval_env, agent.policy, num_eval_episodes)
+returns = [avg_return]
+
+# Reset the environment.
+time_step = train_py_env.reset()
+
+# Create a driver to collect experience.
+collect_driver = py_driver.PyDriver(
+    env,
+    py_tf_eager_policy.PyTFEagerPolicy(
+      agent.collect_policy, use_tf_function=True),
+    [rb_observer],
+    max_steps=collect_steps_per_iteration)
+
+for _ in range(num_iterations):
+
+  # Collect a few steps and save to the replay buffer.
+  time_step, _ = collect_driver.run(time_step)
+
+  # Sample a batch of data from the buffer and update the agent's network.
+  experience, unused_info = next(iterator)
+  train_loss = agent.train(experience).loss
+
+  step = agent.train_step_counter.numpy()
+
+  if step % log_interval == 0:
+    print('step = {0}: loss = {1}'.format(step, train_loss))
+
+  if step % eval_interval == 0:
+    avg_return = compute_avg_return(eval_env, agent.policy, num_eval_episodes)
+    print('step = {0}: Average Return = {1}'.format(step, avg_return))
+    returns.append(avg_return)
